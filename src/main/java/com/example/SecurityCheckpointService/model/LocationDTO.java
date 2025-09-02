f12b68dbe510941cc729aa38bbcd765c827c1bd9
ferry03 <ferrykouemo01@gmail.com>
2025-09-02 15:41:45 +0100

    Ajout du client Feign pour appeler LocationService

---
+++ src/main/java/com/example/SecurityCheckpointService/model/LocationDTO.java
@@ -0,0 +1,9 @@
+package com.example.SecurityCheckpointService.model;
+import lombok.Data;
+
+@Data
+public class LocationDTO {
+    private Long id;
+    private String name;
+    private String address;
+}
